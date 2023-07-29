9f86136b9f2736d02444c5766dc4b714c765f26e
LAVA KUMAR <lavakumar.kushi8197@gmail.com>
2023-07-29 19:31:01 +0530

    add : adding new screens

---
+++ frontend/src/pages/Signup/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Signup.css';
+function Signup() {
+  return (
+    <div className='signup'>
+      <div className="signup-container-bg"></div>
+      <div className="signup-container-main">
+        signup
+      </div>
+    </div>
+  )
+}
+
+export default Signup
