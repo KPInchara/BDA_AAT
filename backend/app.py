22ee02b61a97cac3c8c60d64c0a6f2dbceb5678d
LAVA KUMAR <lavakumar.kushi8197@gmail.com>
2023-07-29 13:59:04 +0530

    import/export of dataset api

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,send_file
 from flask_cors import CORS
 from pymongo import MongoClient
 import os
@@ -8,10 +8,12 @@ import json
 from bson import json_util
 
 #Connect to MongoDB
+client=None
 def connect_to_mongodb():
     try:
+        global client
         client = MongoClient('mongodb+srv://LAVA:Lava7259@cluster0.ltqx9vy.mongodb.net/?retryWrites=true&w=majority/',w='majority')
-        db = client['test']
+        db = client['BDA']
         return db
     except MongoClient.errors.ConnectionFailure as e:
         print(f"Connection error: {e}")
@@ -24,11 +26,19 @@ if db_connection is not None:
 else:
     print("Failed to connect to MongoDB.")
 
+#getting all collection names
+def get_all_collections(database_name):
+    db_connection = client[database_name]
+    collections_list = db_connection.list_collection_names()
+    return collections_list
+ 
+#getting database names
+def get_all_databases():
+    database_names = client.list_database_names()    
+    return database_names
+
 app = Flask(__name__)
 CORS(app)
-
-UPLOAD_FOLDER = r'C:\Users\lavak\Documents\MTech\sem-2\BDT\AAT\backend\upload'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 #ROUTES
 @app.route('/', methods=['GET'])
@@ -47,19 +57,34 @@ def upload_file():
         return 'No selected file', 400
 
     if file:
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        # Save the new .csv file, overwriting the old one if it exists
-        #file.save(os.path.join(UPLOAD_FOLDER, 'data.csv'))
-        
+        # Save the file to a specific folder
+        folder_path = r'C:\Users\lavak\Documents\MTech\sem-2\BDA_AAT\backend\upload'
+        file_path=os.path.join(folder_path, file.filename)
+        file.save(file_path)
         # Read the CSV data using pandas
-        path =os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        # Replace 'data.csv' with the path to your CSV file
-        data = pd.read_csv(path)
+        data = pd.read_csv(file_path)
 
         # Convert the data to a list of dictionaries for MongoDB insertion
         data_list = data.to_dict(orient='records')
-        result = collection.insert_many(data_list)
-        return jsonify({"data":"uploaded to database"}), 200
+        
+        # db_connection.create_collection( os.path.splitext(file.filename)[0])
+        # new_collection=db_connection[ os.path.splitext(file.filename)[0]]
+        # result = new_collection.insert_many(data_list)    
+        try:
+            existing_collections = get_all_collections("BDA")
+            if os.path.splitext(file.filename)[0] in existing_collections:
+                new_collection=db_connection[ os.path.splitext(file.filename)[0]]
+                result = new_collection.insert_many(data_list)
+                #os.remove(file_path)        
+                return jsonify({"data":f"uploaded to database for existing'{os.path.splitext(file.filename)[0]}' collection"}), 200
+            else:
+                db_connection.create_collection( os.path.splitext(file.filename)[0])
+                new_collection=db_connection[ os.path.splitext(file.filename)[0]]
+                result = new_collection.insert_many(data_list)
+                #os.remove(file_path)        
+                return jsonify({"data":f"uploaded to database for '{os.path.splitext(file.filename)[0]}' collection"}), 200
+        except Exception as e:
+            return "upload failed"
     else:
         return 'File upload failed', 500
 
@@ -69,25 +94,39 @@ def download_file():
     global selected_option
     if request.method == 'POST':
         data = request.get_json()
-        selected_option = data['selected_option']
+        db_name= data['db_name']
+        collection_name = data['collection_name']
         
-        collection = db_connection [selected_option]
+        db = client[db_name]
+        collection = db [collection_name]
         data = collection.find()
         
         # Export the data to a JSON file
-        with open('exported_data.json', 'w') as file:
+        file_path=r'C:\Users\lavak\Documents\MTech\sem-2\BDA_AAT\backend\import\exported_data.json'
+        with open(file_path, 'w') as file:
             for document in data:
                 # Convert ObjectId to JSON-serializable format
                 document_json = json.loads(json_util.dumps(document))
                 json.dump(document_json, file)
                 file.write('\n')
-        return jsonify({'message': 'Option successfully set'}), 200
+        return send_file(file_path, as_attachment=True)
     # elif request.method == 'GET':
-    #     return jsonify({'selected_option': selected_option}), 200
+    #     return jsonify({'collection_name':collection_name}), 200
     else:
         return jsonify({'error': 'Invalid request method'}), 400
     
+@app.route('/databaseNames', methods=['GET'])
+def get_lists():
+    databaseNames= get_all_databases()
+    return jsonify({"databaseNames":databaseNames}),200
 
+@app.route('/get_collections', methods=['GET'])
+def get_collections():
+    database_name = request.args.get('db')
+    if database_name is None:
+        return jsonify({'message': 'Database name parameter is missing'}), 400
+    get_collections=get_all_collections(database_name)
+    return jsonify({'collections': get_collections}),200
 
 
 
