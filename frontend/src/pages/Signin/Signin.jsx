0fc0e630736e4b8bb069e620a4a9d3dabaf1e43d
KPInchara <incharakp24@gmail.com>
2023-08-02 22:41:06 +0530

    second commit

--- frontend/src/pages/Signin/Signin.jsx
+++ frontend/src/pages/Signin/Signin.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Signin.css';
-function Signin() {
-  return (
-    <div className='signin'>
-    <div className="signin-container-bg"></div>
-    <div className="signin-container-main">
-      signin
-    </div>
-  </div>
-  )
-}
 
-export default Signin
+const Signin = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   
+    console.log(formData);
+  };
+
+  return (
+    <div className="signin-container">
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-group"> 
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          
+        </div>
+
+        <center><button type="submit">Sign In</button> </center>
+      </form>
+    </div>
+  );
+};
+
+export default Signin;
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,14 +1,96 @@
-import React from 'react'
+import React, { Component, useState } from "react";
 import './Signup.css';
-function Signup() {
+
+export default function Signup() {
+  const [Username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userType, setUserType] = useState("");
+  const [secretKey, setSecretKey] = useState("");
+
+  const handleSubmit = (e) => {
+    if (userType == "Admin" && secretKey != "Inchara") {
+      e.preventDefault();
+      alert("Invalid Admin");
+    } else {
+      e.preventDefault();
+
+
+      console.log(Username, email, password);
+      fetch("http://localhost:3000/Signin", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          Username,
+          email,
+          password,
+          userType,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data, "userRegister");
+          if (data.status == "ok") {
+            alert("Registration Successful");
+          } else {
+            alert("Something went wrong");
+          }
+        });
+    }
+  };
+
   return (
-    <div className='signup'>
-      <div className="signup-container-bg"></div>
-      <div className="signup-container-main">
-        signup
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <form onSubmit={handleSubmit}>
+          <h3>Sign Up</h3>
+          
+
+          <div className="mb-3">
+            <label> Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Sign Up
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            Already registered <a href="/Signin">Signin?</a>
+          </p>
+        </form>
       </div>
     </div>
-  )
+  );
 }
-
-export default Signup
